b2c1eacc1651149d08146bfe3958b2dfb12e74dd
Lora <49145113+LoraOrliGeo@users.noreply.github.com>
2019-07-03 20:32:29 +0300

    remove package

--- Need For Speed/CrossMotorcycle.java
+++ Need For Speed/CrossMotorcycle.java
@@ -1,5 +1,3 @@
-package Inheritance.P04_NeedForSpeed;
-
 public class CrossMotorcycle extends Motorcycle {
     public CrossMotorcycle(double fuel, int horsePower) {
         super(fuel, horsePower);
